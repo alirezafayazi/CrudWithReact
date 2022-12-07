ada78978fce9ee0a57214f4f42f9a60e9a96a4c5
alirezafayazi <alirezm81@gmail.com>
2022-12-07 12:20:20 +0330

    moadl component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/css/Toast.css'
+import './assets/css/main.css'
 import Modal from './components/Modal'
 import Button from 'react-bootstrap/Button'
 import {useState, useEffect} from "react";
@@ -7,13 +7,14 @@ import Api from "./api/api"
 import modal from "./components/Modal";
 
 
-
 function App() {
     const [Data, setData] = useState([]);
-    const [deleteShow, setDeleteShow] = useState(false);
+    const [deleteShow, setDeleteShow] = useState(
+        false);
     const [updateShow, setUpdateShow] = useState(false);
     const [user, setUser] = useState({});
-    const [updateData, setUpdateData] = useState({})
+    const [updateData, setUpdateData] = useState({});
+    const [addShow, setAddShow] = useState(false);
 
     useEffect(() => {
         Api.get('users')
@@ -37,11 +38,11 @@ function App() {
                 setUpdateShow={setUpdateShow}
                 updateData={updateData}
                 setUpdateData={setUpdateData}
+                setAddShow={setAddShow}
+                addShow={addShow}
             >
 
             </Modal>
-
-
 
 
             <div className="row d-flex justify-content-center mt-5">
@@ -51,29 +52,35 @@ function App() {
                         <div
                             className="bg-secondary bg-opacity-25 m-2 p-2 col-3 rounded-3 d-flex flex-column justify-content-center ">
                             <div className='align-self-center'>
-                                <img src={data.avatar} className='rounded-5 img-fluid'/>
+                                <img src={data.avatar} className='rounded-5 img-fluid mw-100 h-auto profile-img  '/>
                             </div>
                             <div className='p-2 align-self-center'>
                                 {data.first_name}
                                 <br/>
                                 {data.last_name}
                             </div>
-                            <div className='d-flex justify-content-around'>
-                                <button className='rounded-3 border border-3 border-danger  bg-opacity-10 px-2 py-1 '
+                            <div className='d-flex justify-content-around flex-lg-row flex flex-sm-column '>
+                                <div className='align-self-sm-center'>
+                                    <button className='rounded-3 border border-3 border-danger bg-opacity-10 px-2 py-1 '
+                                            onClick={() => {
+                                                setDeleteShow(true)
+                                                setUser(data)
+                                            }}>
+                                        Delete
+                                    </button>
+                                </div>
+
+                                <div className='align-self-sm-center '>
+                                    <button
+                                        className='rounded-3 border border-3 border-success bg-opacity-10 rounded-3 px-2 py-1'
                                         onClick={() => {
-                                            setDeleteShow(true)
+                                            setUpdateShow(true)
                                             setUser(data)
                                         }}>
-                                    Delete
-                                </button>
-                                <button
-                                    className='rounded-3 border border-3 border-success bg-opacity-10 rounded-3 px-2 py-1'
-                                    onClick={() =>{
-                                        setUpdateShow(true)
-                                        setUser(data)
-                                    }}>
-                                    Update
-                                </button>
+                                        Update
+                                    </button>
+                                </div>
+
                             </div>
 
                         </div>
@@ -82,7 +89,8 @@ function App() {
                 }
             </div>
             <div className='d-flex justify-content-center container-fluid'>
-                <button className='m-3 border-0 rounded-3 bg-primary text-white w-100 py-2'>
+                <button className='m-3 border-0 rounded-3 bg-primary text-white w-100 py-2'
+                        onClick={() => setAddShow(true)}>
                     Add User
                 </button>
             </div>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,7 +8,7 @@ function Modals(props) {
 
     const UserDelete = () => {
         props.setDeleteShow(false)
-        //delete from database with api
+        //  delete from database with api
         Api.delete(`users/${props.user.id}`)
             .then((res) => console.log(res.status))
             .catch((err) => console.log(err.status))
@@ -16,19 +16,32 @@ function Modals(props) {
         props.setData(props.data.filter((u) => u.id !== props.user.id))
     };
 
-    const UserUpdate= () => {
+    const UserUpdate = () => {
         props.setUpdateShow(false);
-        //update from database with api
-        Api.patch(`users/${props.user.id}`,props.updateData)
-            .then((res) => {console.log(res)})
-            .catch((err) => {console.log(err)})
+        //  update user on database with api
+        Api.patch(`users/${props.user.id}`, props.updateData)
+            .then((res) => {
+                console.log(res)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
 
         const index = props.data.indexOf(props.user);
-        props.data[index] = {...props.user,...props.updateData};
+        props.data[index] = {...props.user, ...props.updateData};
+    }
+    const UserAdd=()=>{
+        props.setAddShow(false);
+        //    add user on database with API
+        Api.post('users/',props.updateData)
+            .then((res)=>{console.log(res)})
+            .catch((err)=>{console.log(err)});
+        console.log(props.updateData)
+        props.setData([...props.data , props.updateData]);
     }
     return (
         <>
-             {/*modal for delete users*/}
+            {/*modal for delete users*/}
             <Modal show={props.deleteShow} onHide={() => props.setDeleteShow(false)}>
                 <Modal.Header closeButton>
                     <Modal.Title>Delete User</Modal.Title>
@@ -46,8 +59,8 @@ function Modals(props) {
                 </Modal.Footer>
             </Modal>
 
-            {/*modal for update user*/}
-            <Modal show={props.updateShow} onHide={()=>props.setUpdateShow(false)}>
+            {/***modal for update user***/}
+            <Modal show={props.updateShow} onHide={() => props.setUpdateShow(false)}>
                 <Modal.Header closeButton>
                     <Modal.Title>Update User</Modal.Title>
                 </Modal.Header>
@@ -56,27 +69,30 @@ function Modals(props) {
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                             <Form.Label>First name</Form.Label>
                             <Form.Control
-                                onChange={e=> props.setUpdateData({
+                                onChange={e => props.setUpdateData({
                                     ...props.updateData,
-                                    "first_name":e.target.value})}
+                                    "first_name": e.target.value
+                                })}
                                 type="text"
                                 placeholder="your First name"
                                 autoFocus
                             />
                             <Form.Label className='mt-2'>Last name</Form.Label>
                             <Form.Control
-                                onChange={e=> props.setUpdateData({
+                                onChange={e => props.setUpdateData({
                                     ...props.updateData,
-                                    "last_name":e.target.value})}
+                                    "last_name": e.target.value
+                                })}
                                 type="text"
                                 placeholder="your last name"
                                 autoFocus
                             />
                             <Form.Label className='mt-2'>image link</Form.Label>
                             <Form.Control
-                                onChange={e=> props.setUpdateData({
+                                onChange={e => props.setUpdateData({
                                     ...props.updateData,
-                                    "avatar":e.target.value})}
+                                    "avatar": e.target.value
+                                })}
                                 type="text"
                                 placeholder="your image link"
                                 autoFocus
@@ -86,11 +102,76 @@ function Modals(props) {
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={()=>props.setUpdateShow(false)}>
+                    <Button variant="secondary" onClick={() => props.setUpdateShow(false)}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={()=> {
+                    <Button variant="primary" onClick={() => {
                         UserUpdate();
+                    }}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+            {/***modal for add user***/}
+            <Modal show={props.addShow} onHide={() => props.setAddShow(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add User</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>image link</Form.Label>
+                            <Form.Control
+                                onChange={e => props.setUpdateData({
+                                    "avatar": e.target.value
+                                })}
+                                type="text"
+                                placeholder="your image link"
+                                autoFocus
+                            />
+                            <Form.Label className='mt-2'>First name</Form.Label>
+                            <Form.Control
+                                onChange={e => props.setUpdateData({
+                                    ...props.updateData,
+                                    "first_name": e.target.value
+                                })}
+                                type="text"
+                                placeholder="your First name"
+                                autoFocus
+                            />
+                            <Form.Label className='mt-2'>Last name</Form.Label>
+                            <Form.Control
+                                onChange={e => props.setUpdateData({
+                                    ...props.updateData,
+                                    "last_name": e.target.value
+                                })}
+                                type="text"
+                                placeholder="your last name"
+                                autoFocus
+                            />
+                            <Form.Label className='mt-2'>Email</Form.Label>
+                            <Form.Control
+                                onChange={e => props.setUpdateData({
+                                    ...props.updateData,
+                                    "email": e.target.value
+                                })}
+                                type="email"
+                                placeholder="your email address"
+                                autoFocus
+                            />
+
+
+                        </Form.Group>
+
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => props.setAddShow(false)}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => {
+                        UserAdd();
                     }}>
                         Save Changes
                     </Button>
